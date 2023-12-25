24c4354335022979ac62d89117a5e392d0931630
achyut koirala <aoogleak@gmail.com>
2023-12-25 20:21:25 +0530

    modular with whole folder

--- main.go
+++ main.go
@@ -2,47 +2,51 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/ebitengine/oto/v3"
 	"github.com/hajimehoshi/go-mp3"
 )
 
+// Prepare an Oto context (creating context for single time)
+var op = &oto.NewContextOptions{
+	SampleRate:   44100,
+	ChannelCount: 2,
+	Format:       oto.FormatSignedInt16LE,
+}
+var otoCtx, readyChan, err = oto.NewContext(op)
+
 func main() {
-	// TODO: open music file  and play
-	// TODO: while playing display visual
+	// TODO:while playing display visual
+	// TODO:custom mp3 decoder and player
 	if len(os.Args) == 1 {
 		panic("Open a .mp3 file.")
 	}
 	// check if it's a single a folder
 	if os.Args[1] == "." {
 		playFolder()
+	} else {
+		mp3File := os.Args[1]
+		play(mp3File)
 	}
-	play()
-	mp3File := os.Args[1]
+}
 
-	// Open the file for reading. Do NOT close before you finish playing!
+func play(mp3File string) {
 	file, err := os.Open(mp3File)
 	if err != nil {
 		panic("opening " + mp3File + " failed: " + err.Error())
 	}
 
-	fmt.Printf("GoPlay.mp3 NOW NPLAYING %s", mp3File)
+	fmt.Printf("-------- NOW NPLAYING %s --------\n", mp3File)
 	// Decode file. This process is done as the file plays so it won't load whole thing into memory
 	decodedMp3, err := mp3.NewDecoder(file)
 	if err != nil {
 		panic("mp3.NewDecoder failed: " + err.Error())
 	}
-
-	// Prepare an Oto context (this will use your default audio device) that will play
-	op := &oto.NewContextOptions{}
-	op.SampleRate = 44100 // usually 44100 or 48000
-	op.ChannelCount = 2
-	op.Format = oto.FormatSignedInt16LE
-
-	// Remember that you should **not** create more than one context
-	otoCtx, readyChan, err := oto.NewContext(op)
 	if err != nil {
 		panic("oto.NewContext failed: " + err.Error())
 	}
@@ -56,16 +60,34 @@ func main() {
 		time.Sleep(time.Millisecond)
 	}
 	err = player.Close()
+
 	if err != nil {
 		panic("player.Close failed: " + err.Error())
 	}
 	file.Close()
 }
 
-func play() {
-
-}
-
 func playFolder() {
+	fmt.Printf("All Music from Current folder added to Playlist.\n")
+	fileInfos, err := os.ReadDir(".")
+	if err != nil {
+		log.Fatal("Couldn't Read from Current Directory!\n")
+	}
 
+	var mp3Files []string
+	for _, file := range fileInfos {
+		ext := filepath.Ext(file.Name())
+		if ext == ".mp3" {
+			mp3Files = append(mp3Files, file.Name())
+		}
+	}
+	for i, file := range mp3Files {
+		fmt.Printf("%d. %s  ", i+1, file)
+	}
+	fmt.Printf("\n")
+	for range mp3Files {
+		// Shuffle Mode
+		songNum := rand.Intn(len(mp3Files))
+		play(mp3Files[songNum])
+	}
 }
